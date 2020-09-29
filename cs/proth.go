aa0516ed37728b6f48c9826da0ed91a46d0f675a
Gbenga <omg.tosine@gmail.com>
2020-09-29 11:56:43 +0100

    fixed comments and naming

--- cs/proth.go
+++ cs/proth.go
@@ -6,20 +6,18 @@ import (
 	"strconv"
 )
 
-
-func isProth(in int) bool {
+func isProth(number int) bool {
 	// A Proth number is a number N of the form N=k(2^n)+1
 
 	// deduct 1 from the number to give a number of the form k(2^n)
-	n := in - 1
+	n := number - 1
 	// loop through all odd numbers from k = 1 to n/k
-	// check if can can divide n in a way that n/k is a power of 2
+	// check if k can divide n in a way that n/k is a power of 2
 	for k := 1; k < (n / k); k += 2 {
 		// Check if n is divisible by k
 		if n%k == 0 {
 			// Check if n/k is a power of two
 			if isPowerOfTwo(n / k) {
-				fmt.Printf("%v is a Proth number, ", in)
 				return true
 			}
 		}
@@ -43,12 +41,13 @@ func isPrime(n int) bool {
 func isProthPrime(n int) bool {
 	// if n is a proth number then check if it is a prime number
 	if isProth(n) {
+		fmt.Printf("%v is a Proth number, \n", n)
 		if isPrime(n) {
 			return true
 		}
 		return false
 	}
-	fmt.Printf("%v is not a Proth number, ", n)
+	fmt.Printf("%v is not a Proth number, \n", n)
 	return false
 }
 
